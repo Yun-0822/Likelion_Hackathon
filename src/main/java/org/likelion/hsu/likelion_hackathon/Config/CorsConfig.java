0164a97158713c417aa58c53f1883e31c6d02081
재민 <jjm0203311@naver.com>
2025-08-24 14:30:13 +0900

    Nginx설정

--- src/main/java/org/likelion/hsu/likelion_hackathon/Config/CorsConfig.java
+++ src/main/java/org/likelion/hsu/likelion_hackathon/Config/CorsConfig.java
@@ -4,15 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//프론트나 다른 도메인에서 오는 요청을 허용할 지 확인
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 경로에 대해 CORS를 허용하겠다는 뜻
-                .allowedOrigins("*") // 프론트 주소
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") //프론트에서 보낼 수 있는 HTTP매서드를 허용
-                .allowedHeaders("*") //요청에 어떤 헤더를 포함해도 허용
-                .allowCredentials(false); // 로그인/쿠키 미사용 → false
+        registry.addMapping("/**") // 모든 경로 허용
+                .allowedOrigins(
+                        "https://home-site-133c8.web.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // 인증/쿠키 포함하려면 true (false면 제외)
     }
 }
